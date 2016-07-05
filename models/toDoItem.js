fa3dec8687791fea8b10bfd889d05e7c50c847a5
LukaszGr <lukasz.grychowski@gmail.com>
2016-07-05 12:18:59 +0100

    object can store and retrive a task

---
+++ models/toDoItem.js
@@ -0,0 +1,22 @@
+function ToDoItem() {
+  this.task = "";
+  this.isComplete = false;
+};
+
+ToDoItem.prototype.setTask = function(task) {
+  this.task = task;
+};
+
+ToDoItem.prototype.getTask = function() {
+  return this.task;
+};
+
+ToDoItem.prototype.complete = function() {
+  this.isComplete = true;
+};
+
+ToDoItem.prototype.checkComplete = function() {
+  return this.isComplete;
+};
+module.exports = ToDoItem
+
---
+++ test/to_do_test.js
@@ -0,0 +1,44 @@
+
+var expect = require("chai").expect;
+var ToDoItem = require("../models/toDoItem");
+var toDoItem;
+
+describe ("ToDoItem", function() {
+  beforeEach(function() {
+    toDoItem = new ToDoItem();
+  });
+
+  it("object is instantiated with an empty task and false complete attr", function() {
+    expect(toDoItem.task).to.equal("");
+    expect(toDoItem.isComplete).to.equal(false);
+  });
+
+  describe ("#setTask", function() {
+    it("can store a task", function() {
+      toDoItem.setTask("Eat breakfast");
+      expect(toDoItem.task).to.equal("Eat breakfast");
+    });
+  });
+
+  describe ("#getTask", function() {
+    it("can retrive a task", function() {
+      toDoItem.setTask("Eat breakfast");
+      expect(toDoItem.getTask()).to.equal("Eat breakfast");
+    });
+  });
+
+  describe ("#complete", function() {
+    it("completes a task", function() {
+      toDoItem.complete();
+      expect(toDoItem.isComplete).to.equal(true);
+    });
+  });
+
+  describe ("#checkComplete", function() {
+    it("checks if taks is completed", function() {
+      toDoItem.complete();
+      expect(toDoItem.checkComplete()).to.equal(true);
+    });
+  });
+
+});
