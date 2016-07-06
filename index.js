7003c821baa331eb6dedb78056c53c2d6719673d
paulkenrick <pkenrick@gmail.com>
2016-07-06 18:58:55 +0100

    Feature testing that user can add task and view it in the list

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var toDoList =  new ToDoList();
 toDoList.addTask(ToDoItem, "Go shopping");
-toDoList.addTask(ToDoItem, "Make dinner");
+// toDoList.addTask(ToDoItem, "Make lunch");
+
 
 document.getElementById('list').innerHTML = toDoList.display();
+
+var el = document.getElementById('addTask')
+
+document.getElementById('addButton').addEventListener('click', function (evt) {
+  console.log(el.textInput.value);
+  evt.preventDefault();
+});
--- test/features/featureTest.js
+++ test/features/featureTest.js
@@ -12,7 +12,26 @@ describe("Interface", function() {
     browser.visit('/', done);
   });
   
-  it('can see title', function(){
+  it('can see title', function() {
     browser.assert.text('title', 'To Do List');
   });
+
+  it('can see to do list', function() { 
+    assert.equal(browser.text('li'), 'Go shopping');
+  });
+
+  // it('can submit task then see task displayed', function(done) {
+  //   console.log('1')
+  //   browser.visit('/').then(function(){
+  //         console.log('2')
+  //     browser.fill('task', 'Feed grandma');
+  //     return browser.pressButton('Add Task');
+  //   }).then(function(){
+  //         console.log('3')
+  //     assert(browser.text('#list') === "Feed grandma", "error message");
+  //   }).then(done, done);
+  //       console.log('4')
+  // });
+  
+
 });
