16b97db2a8749beb6f0fdaae978fc14752d6c7e1
paulkenrick <pkenrick@gmail.com>
2016-07-06 12:10:38 +0100

    Small changes

--- models/toDoItem.js
+++ models/toDoItem.js
@@ -1,23 +1,22 @@
-function ToDoItem(task) {
-  this.task = typeof task === 'undefined' ? "" : task;
-  this.isComplete = false;
-};
+  function ToDoItem(task) {
+    this.task = typeof task === 'undefined' ? "" : task;
+    this.isComplete = false;
+  };
 
-ToDoItem.prototype.setTask = function(task) {
-  this.task = task;
-};
+  ToDoItem.prototype.setTask = function(task) {
+    this.task = task;
+  };
 
-ToDoItem.prototype.getTask = function() {
-  return this.task;
-};
+  ToDoItem.prototype.getTask = function() {
+    return this.task;
+  };
 
-ToDoItem.prototype.complete = function() {
-  this.isComplete = true;
-};
+  ToDoItem.prototype.complete = function() {
+    this.isComplete = true;
+  };
 
-ToDoItem.prototype.checkComplete = function() {
-  return this.isComplete;
-};
+  ToDoItem.prototype.checkComplete = function() {
+    return this.isComplete;
+  };
 
-module.exports = ToDoItem
-
+  module.exports = ToDoItem;
